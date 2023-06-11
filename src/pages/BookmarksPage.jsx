b95cf73bb22b94fda1a9378d5876d553ab6fa39f
Emmanuel Festus <120015643+GeneralAike@users.noreply.github.com>
2023-06-11 23:52:56 +0100

    working on bookmark (save state stable)

--- src/pages/BookmarksPage.jsx
+++ src/pages/BookmarksPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import HomeRight from "./Home/HomeRight";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import TweetStream from "./Home/dataStream/TweetStream";
+
+library.add(fas);
+library.add(fab);
+library.add(far);
 
 function BookmarksPage() {
   return (
-    <div>BookmarksPage</div>
-  )
+    <>
+      <section className="homepage-center h-screen relative overflow-hidden">
+        <header className="px-3 py-2 flex items-center justify-between">
+          <div>
+            <p className="text-xl font-semibold">Bookmarks</p>
+            <p className="text-xs bookmarkUserName">@general_ik</p>
+          </div>
+          <div className="cursor-pointer p-2 bookmarkElips flex justify-center items-center rounded-full">
+            <FontAwesomeIcon icon="fa-solid fa-ellipsis" />
+          </div>
+        </header>
+        <section>
+            <TweetStream/>
+        </section>
+      </section>
+
+      <HomeRight />
+    </>
+  );
 }
 
-export default BookmarksPage
+export default BookmarksPage;
